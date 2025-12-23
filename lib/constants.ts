372eeadadb54810bd36aad7551f8d9d37a603ad3
Marathoner-dev <marathoner.dev@gmail.com>
2025-12-24 08:12:05 +0900

    add admin email

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,6 +4,6 @@ export const USER_PASSWORD = 'escapeneon';
 // 관리자 이메일 허용 목록
 export const ADMIN_EMAILS = [
   'marathoner.dev@gmail.com', // 여기에 관리자 이메일을 추가하세요
-  // 추가 관리자 이메일을 여기에 추가할 수 있습니다
+  'hyunbin1089@gmail.com'
 ];
 
